12f868127b3d94d73991be525dc6e6c6947cfa4d
Thomas Bueno <thomasscolax@gmail.com>
2020-05-11 14:48:03 -0300

    add Animate Scroll
    Add animation to scroll, I made a calculation to show these animations only in 60% instead of 100%, if < 0 do the action, otherwise, remove the active class

--- Fantastic_Animals/javascripts/script.js
+++ Fantastic_Animals/javascripts/script.js
@@ -65,3 +65,26 @@ function initScrollSmooth() {
 }
 
 initScrollSmooth();
+
+function initAnimationScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+  if (sections.length) {
+    const halfWindow = window.innerHeight * 0.6;
+
+    function animateScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top - halfWindow;
+
+        if (sectionTop < 0) {
+          section.classList.add("ativo");
+        } else section.classList.remove("ativo");
+      });
+    }
+
+    animateScroll();
+
+    window.addEventListener("scroll", animateScroll);
+  }
+}
+
+initAnimationScroll();
